4d6e60524f1ada8180ebb6aa3785ba6b42104464
Allan de Melo Heil <allanheil1@gmail.com>
2025-01-28 20:23:49 -0300

    CardPopularProducts

---
+++ client/src/app/dashboard/CardSalesSummary.tsx
@@ -0,0 +1,17 @@
+import { useGetDashboardMetricsQuery } from "@/state/api";
+import React, { useState } from "react";
+
+const CardSalesSummary = () => {
+  const { data, isLoading, isError } = useGetDashboardMetricsQuery();
+  const saleData = data?.salesSummary || [];
+
+  const [timeframe, setTimeFrame] = useState("weekly");
+
+  if (isError) return <div className="m-5">Failed to fetch data</div>;
+
+  return (
+    <div className="row-span-3 xl:row-span-6 bg-gray-500">CardSalesSummary</div>
+  );
+};
+
+export default CardSalesSummary;
